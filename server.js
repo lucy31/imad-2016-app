d19754b1952e5aecf9ad2b6067dc7c5152eff3a5
lucy31 <leocub96@gmail.com>
2016-11-02 16:12:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,26 +19,34 @@ var ArticleOne={
         </p>`
 };
 
-var htmlTemplate=`
-
-<html>
-  <head>
-    <title${title}</title>
-    <meta name="viewport" contents="width=device-width, initial-scale=1"/>
-        <link href="/ui/style.css" rel="stylesheet" />
-  </head>
-  <body>
-    <div class="container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>${heading}</h3>
-            ${content}
-    </div>
-  </body>
-</html>
+function createTemplate(data) {
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlTemplate=`
+    
+        <html>
+          <head>
+            <title${title}</title>
+            <meta name="viewport" contents="width=device-width, initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
+          </head>
+          <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>
+                    ${content}
+                </div>
+            </div>
+          </body>
+        </html>
 `;
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
