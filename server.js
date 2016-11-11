483e6a6bdd5f9970401a948315f164cf4b93443f
lucy31 <leocub96@gmail.com>
2016-11-11 10:25:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(article[articleName]));
 });
 
-app.get('/ui/Indian.html', function (req, res) {
+app.get('/Indian', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Indian.html'));
 });
 
