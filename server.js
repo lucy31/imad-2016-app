75be81d67341dbd9ce0bb1de832aaebc9c06dfaf
lucy31 <leocub96@gmail.com>
2016-11-12 02:06:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -368,6 +368,34 @@ app.get('/ui/samosa.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'samosa.jpg'));
 });
 
+app.get('/ui/kulfi.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'kulfi.jpg'));
+});
+
+app.get('/ui/gulabjamun.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'gulabjamun.jpg'));
+});
+
+app.get('/ui/rasgulla.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'rasgulla.jpg'));
+});
+
+app.get('/ui/ladoo.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'ladoo.jpg'));
+});
+
+app.get('/ui/kajubarfi.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'kajubarfi.jpg'));
+});
+
+app.get('/ui/payasam.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'payasam.jpg'));
+});
+
+app.get('/ui/phirni.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'phirni.jpg'));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
