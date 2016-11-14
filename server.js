a7098b6a4550c68da86cb8d2d90899fc1925fea4
lucy31 <leocub96@gmail.com>
2016-11-14 13:11:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,9 @@ app.post('/login' ,function (req,res){
             if (err) {
                 res.status(500).send(err.toString());
             } else {
+                if( result.rows.length === 0){
+                    res.send(400)
+                }
             res.send('User successfully created: '+ userid);
             }
     });    
