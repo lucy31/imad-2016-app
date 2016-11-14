bd267f08091c8273a486d408e569d1e583b662a8
lucy31 <leocub96@gmail.com>
2016-11-14 14:53:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,7 @@ app.post('/login' ,function (req,res){
                 else{
                     var dbString = result.rows[0].password;
                     var salt = dbString.split('$')[2];
+                    var hashedPassword = hash(password,salt);
             res.send('User successfully created: '+ userid);
                 }
             }
