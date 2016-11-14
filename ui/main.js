260084f2465b88c41f9b2d60a6bb5c6e4af588f8
lucy31 <leocub96@gmail.com>
2016-11-14 16:19:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,22 @@
-console.log('Loaded!');
+var submit = document.getElementById('submit');
+submit.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names=request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i<names.length;i++){
+                    list == '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    
+    var userid = document.getElementById('userid').value;
+    var password = document.getElementById('password').value;
+    re
+}
